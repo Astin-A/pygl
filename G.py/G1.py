938f5128b6dee40ac4a4635582b2cff0c97e7909
Ali Astin Azarafza <astinazarafza@gmail.com>
2024-02-25 06:02:16 -0500

    creating the mainloop
    we check the event queue if we get a exit event then we quit the game loop to refresh the screen we clear the color buffer
    we update the screen by flipping the buffers and we tick our clock over holding the famerate at 60fps

--- G.py/G1.py
+++ G.py/G1.py
@@ -6,9 +6,26 @@ class App:
 
 
     #init python
-    def __init__(self) -> None:
+    def __init__(self):
         pg.init()
         pg.display.set_mode((640, 480), pg.OPENGL|pg.DOUBLEBUF)
         self.clock = pg.time.Clock()
         #init opengl
         glClearColor(0.1, 0.2, 0.2, 1)
+        self.mainLoop()
+
+
+    def mainLoop(self):
+        running = True
+        while (running):
+            #check events 
+            for event in pg.event.get():
+                if (event.type == pg.quit):
+                    running = False
+            #refresh screen
+            glClear(GL_COLOR_BUFFER_BIT)
+            pg.display.flip()
+
+            #time
+            self.clock.tick(60)
+             
