c510efdc766df57e53bb2d99d5b022262922a199
Ali Astin Azarafza <astinazarafza@gmail.com>
2024-04-24 17:07:35 -0500

    meh

--- Texturs/G4.py
+++ Texturs/G4.py
@@ -17,6 +17,87 @@ def create_shader(vertex_filepath: str, fragment_filepath: str) -> int:
     
     return shader
 
+def loadMesh(filename: str) -> list[float]:
+
+    v = []
+    vt = []
+    vn = []
+
+    vertices = []
+
+    with open(filename, "r") as file:
+
+        line = file.readline()
+
+        while line:
+
+            words = line.split(" ")
+            match words[0]:
+            
+                case "v":
+                    v.append(read_vertex_data(words))
+
+                case "vt":
+                    vt.append(read_texcoord_data(words))
+                
+                case "vn":
+                    vn.append(read_normal_data(words))
+            
+                case "f":
+                    read_face_data(words, v, vt, vn, vertices)
+            
+            line = file.readline()
+
+    return vertices
+    
+def read_vertex_data(words: list[str]) -> list[float]:
+    return [
+        float(words[1]),
+        float(words[2]),
+        float(words[3])
+    ]
+    
+def read_texcoord_data(words: list[str]) -> list[float]:
+
+    return [
+        float(words[1]),
+        float(words[2])
+    ]
+    
+def read_normal_data(words: list[str]) -> list[float]:
+
+    return [
+        float(words[1]),
+        float(words[2]),
+        float(words[3])
+    ]
+
+def read_face_data(
+    words: list[str], 
+    v: list[list[float]], vt: list[list[float]], 
+    vn: list[list[float]], vertices: list[float]) -> None:
+
+    triangleCount = len(words) - 3
+
+    for i in range(triangleCount):
+
+        make_corner(words[1], v, vt, vn, vertices)
+        make_corner(words[2 + i], v, vt, vn, vertices)
+        make_corner(words[3 + i], v, vt, vn, vertices)
+    
+def make_corner(corner_description: str, 
+    v: list[list[float]], vt: list[list[float]], 
+    vn: list[list[float]], vertices: list[float]) -> None:
+
+    v_vt_vn = corner_description.split("/")
+    
+    for element in v[int(v_vt_vn[0]) - 1]:
+        vertices.append(element)
+    for element in vt[int(v_vt_vn[1]) - 1]:
+        vertices.append(element)
+    for element in vn[int(v_vt_vn[2]) - 1]:
+        vertices.append(element)
+
 class Entity:
 
     def __init__(self, position: list[float], eulers: list[float]):
@@ -54,7 +135,7 @@ class Entity:
 class App:
 
     def __init__(self):
- 
+
         self._set_up_pygame()
 
         self._set_up_timer()
@@ -66,8 +147,7 @@ class App:
         self._set_onetime_uniforms()
 
         self._get_uniform_locations()
-
-
+    
     def _set_up_pygame(self) -> None:
 
         pg.init()
@@ -80,12 +160,12 @@ class App:
     def _set_up_timer(self) -> None:
 
         self.clock = pg.time.Clock()
-
+    
     def _set_up_opengl(self) -> None:
 
         glClearColor(0.1, 0.2, 0.2, 1)
         glEnable(GL_DEPTH_TEST)
-    
+
     def _create_assets(self) -> None:
 
         self.cube = Entity(
@@ -119,6 +199,7 @@ class App:
         self.modelMatrixLocation = glGetUniformLocation(self.shader,"model")
     
     def run(self) -> None:
+        """ Run the app """
 
         running = True
         while (running):
@@ -127,16 +208,14 @@ class App:
                 if (event.type == pg.QUIT):
                     running = False
             
-            #update cube
             self.cube.update()
             
             #refresh screen
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
             glUseProgram(self.shader)
 
-            
             glUniformMatrix4fv(
-                self.modelMatrixLocation, 1, GL_FALSE, 
+                self.modelMatrixLocation,1,GL_FALSE,
                 self.cube.get_model_transform())
             self.wood_texture.use()
             self.cube_mesh.arm_for_drawing()
@@ -155,94 +234,12 @@ class App:
         glDeleteProgram(self.shader)
         pg.quit()
 
-class CubeMesh:
-
-    def __init__(self):
-
-
-        # x, y, z, s, t
-        vertices = (
-            -0.5, -0.5, -0.5, 0, 0,
-             0.5, -0.5, -0.5, 1, 0,
-             0.5,  0.5, -0.5, 1, 1,
-
-             0.5,  0.5, -0.5, 1, 1,
-            -0.5,  0.5, -0.5, 0, 1,
-            -0.5, -0.5, -0.5, 0, 0,
-
-            -0.5, -0.5,  0.5, 0, 0,
-             0.5, -0.5,  0.5, 1, 0,
-             0.5,  0.5,  0.5, 1, 1,
-
-             0.5,  0.5,  0.5, 1, 1,
-            -0.5,  0.5,  0.5, 0, 1,
-            -0.5, -0.5,  0.5, 0, 0,
-
-            -0.5,  0.5,  0.5, 1, 0,
-            -0.5,  0.5, -0.5, 1, 1,
-            -0.5, -0.5, -0.5, 0, 1,
-
-            -0.5, -0.5, -0.5, 0, 1,
-            -0.5, -0.5,  0.5, 0, 0,
-            -0.5,  0.5,  0.5, 1, 0,
-
-             0.5,  0.5,  0.5, 1, 0,
-             0.5,  0.5, -0.5, 1, 1,
-             0.5, -0.5, -0.5, 0, 1,
-
-             0.5, -0.5, -0.5, 0, 1,
-             0.5, -0.5,  0.5, 0, 0,
-             0.5,  0.5,  0.5, 1, 0,
-
-            -0.5, -0.5, -0.5, 0, 1,
-             0.5, -0.5, -0.5, 1, 1,
-             0.5, -0.5,  0.5, 1, 0,
-
-             0.5, -0.5,  0.5, 1, 0,
-            -0.5, -0.5,  0.5, 0, 0,
-            -0.5, -0.5, -0.5, 0, 1,
-
-            -0.5,  0.5, -0.5, 0, 1,
-             0.5,  0.5, -0.5, 1, 1,
-             0.5,  0.5,  0.5, 1, 0,
-
-             0.5,  0.5,  0.5, 1, 0,
-            -0.5,  0.5,  0.5, 0, 0,
-            -0.5,  0.5, -0.5, 0, 1
-        )
-        self.vertex_count = len(vertices)//5
-        vertices = np.array(vertices, dtype=np.float32)
-
-        self.vao = glGenVertexArrays(1)
-        glBindVertexArray(self.vao)
-        self.vbo = glGenBuffers(1)
-        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
-        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
-
-        glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(0))
-
-        glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 20, ctypes.c_void_p(12))
-    
-    def arm_for_drawing(self) -> None:
-
-        glBindVertexArray(self.vao)
-    
-    def draw(self) -> None:
-
-        glDrawArrays(GL_TRIANGLES, 0, self.vertex_count)
-
-    def destroy(self) -> None:
-
-        glDeleteVertexArrays(1,(self.vao,))
-        glDeleteBuffers(1,(self.vbo,))
-
 class Mesh:
 
     def __init__(self, filename: str):
+
         # x, y, z, s, t, nx, ny, nz
-        vertices = self.load_mesh(filename)
+        vertices = loadMesh(filename)
         self.vertex_count = len(vertices)//8
         vertices = np.array(vertices, dtype=np.float32)
 
@@ -260,25 +257,24 @@ class Mesh:
         glEnableVertexAttribArray(1)
         glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 32, ctypes.c_void_p(12))
     
-    def load_mesh(self, filename: str) -> list[float]:
-        with open (filename, "r") as file: 
-            line = file.readline()
-            while line:
-                words = line.split(" ")
-                if words[0] == 'v':
-                    print(f"{line} is a veryex line")
-                line = file.readline()
-        
-        return []
+    def arm_for_drawing(self) -> None:
+        glBindVertexArray(self.vao)
+    
+    def draw(self) -> None:
+        glDrawArrays(GL_TRIANGLES, 0, self.vertex_count)
 
     def destroy(self) -> None:
-        
+        glDeleteVertexArrays(1,(self.vao,))
+        glDeleteBuffers(1,(self.vbo,))
+    
+    def destroy(self) -> None:
         glDeleteVertexArrays(1,(self.vao,))
         glDeleteBuffers(1,(self.vbo,))
 
 class Material:
 
     def __init__(self, filepath: str):
+
         self.texture = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, self.texture)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
@@ -292,10 +288,12 @@ class Material:
         glGenerateMipmap(GL_TEXTURE_2D)
 
     def use(self) -> None:
+
         glActiveTexture(GL_TEXTURE0)
         glBindTexture(GL_TEXTURE_2D,self.texture)
 
     def destroy(self) -> None:
+
         glDeleteTextures(1, (self.texture,))
 
 my_app = App()
